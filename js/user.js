92cc407d09cc9bb8b5e4ee2d762f50f6fff8c332
LordotU <LevshinO@gmail.com>
2012-03-20 15:26:18 +0400

    Some cosmetic changes in user.js.

--- js/user.js
+++ js/user.js
@@ -4,8 +4,6 @@
  * @param {string} String value of route JSON object
  */
 function drawRoute(json_value) {
-  polyline = new YMaps.Polyline(); //Init polyline
-
   //Init polyline style
   var ps = new YMaps.Style();
   ps.lineStyle = new YMaps.LineStyle();
@@ -14,22 +12,21 @@ function drawRoute(json_value) {
 
   YMaps.Styles.add("ymaps_routes_polyline", ps);
 
-  polyline.setStyle("ymaps_routes_polyline");
-  polyline.setOptions({hasBalloon: false});
-
-  map.addOverlay(polyline); //Add polyline to map
-
   //Init checkpoint marker style
   if(typeof Drupal.settings.ymaps_routes.marker_style != 'undefined') {
     var ms = new YMaps.Style();
     ms.iconStyle = new YMaps.IconStyle();
     ms.iconStyle.href = Drupal.settings.basePath + Drupal.settings.ymaps_routes.marker_style.filepath;
-    console.log(ms.iconStyle.href);
     ms.iconStyle.size = new YMaps.Point(Drupal.settings.ymaps_routes.marker_style.width, Drupal.settings.ymaps_routes.marker_style.height);
     ms.iconStyle.offset = new YMaps.Point(-(Drupal.settings.ymaps_routes.marker_style.width/2), -Drupal.settings.ymaps_routes.marker_style.height);
 
     YMaps.Styles.add("ymaps_routes_marker", ms);
   }
+
+  polyline = new YMaps.Polyline(); //Init polyline
+  polyline.setOptions({hasBalloon: false});
+  polyline.setStyle("ymaps_routes_polyline");
+  map.addOverlay(polyline); //Add polyline to map
 
   var route = JSON.parse(json_value); //Parse @param string to JSON
 
